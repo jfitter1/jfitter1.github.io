42f30e0d04a42812513b1a8b1d9b34c021419933
Jacob Fitterer <JacobFitterer@Jacobs-MacBook-Pro.local>
2018-02-18 10:14:22 -0800

    vendor prefixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,7 +44,7 @@
 // ===== Scroll to Top ==== 
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
-        scrollTop : 100                       // Scroll to top of body
+        scrollTop : 0                       // Scroll to top of body
     }, 500);
 });
 
